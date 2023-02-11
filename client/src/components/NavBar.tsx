0176d3b77e6025ae97087697a39a0293541578c7
Hansol <losnah24@gmail.com>
2023-02-11 19:12:13 +0900

    Link 태그 a 태그 오류 해결

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar: React.FC = () => {
   return (
     <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between px-5 bg-white h-13">
       <span className="text-2xl font-semibold text-gray-400">
-        <Link href="/">
+        <Link href="/" legacyBehavior>
           <Image
             src="/reddit-name-logo.png"
             alt="logo"
@@ -54,15 +54,19 @@ const NavBar: React.FC = () => {
             </button>
           ) : (
             <>
-              <Link href="/login">
-                <a className="w-20 px-2 pt-1 mr-2 text-sm text-center text-blue-500 border border-blue-500 rounded h-7">
+              <Link
+                href="/login"
+                className="w-20 px-2 pt-1 mr-2 text-sm text-center text-blue-500 border border-blue-500 rounded h-7">
+                
                   로그인
-                </a>
+                
               </Link>
-              <Link href="/register">
-                <a className="w-20 px-2 pt-1 text-sm text-center text-white bg-gray-400 rounded h-7">
+              <Link
+                href="/register"
+                className="w-20 px-2 pt-1 text-sm text-center text-white bg-gray-400 rounded h-7">
+                
                   회원가입
-                </a>
+                
               </Link>
             </>
           ))}
