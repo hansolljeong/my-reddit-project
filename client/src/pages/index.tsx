fd2e398aa4b9f780190fcacc4e0448622669792b
Hansol <losnah24@gmail.com>
2023-02-11 20:32:27 +0900

    홈에서 커뮤니티 리스트, 권한에 따라 노출 여부 결정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { useAuthState } from "@/context/auth";
 import { Sub } from "@/types";
 import axios from "axios";
 import Link from "next/link";
 import useSWR from "swr";
+import Image from "next/image";
 
 export default function Home() {
+  const { authenticated } = useAuthState();
+
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
   };
@@ -22,16 +26,41 @@ export default function Home() {
           </div>
 
           {/* 커뮤니티 리스트 */}
-          <div></div>
-
-          <div className="w-full py-6 text-center">
-            <Link
-              href="/subs/create"
-              className="w-full p-2 text-center text-white bg-gray-400 rounded"
-            >
-              커뮤니티 만들기
-            </Link>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <Image
+                    src={sub.imageUrl}
+                    className="rounded-full cursor-pointer"
+                    alt="Sub"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+                <Link
+                  href={`/r/${sub.name}`}
+                  className="ml-2 font-bold hover:cursor-pointer"
+                >
+                  /r/{sub.name}
+                </Link>
+                <p className="ml-auto font-md">{sub.postCount}</p>
+              </div>
+            ))}
           </div>
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link
+                href="/subs/create"
+                className="w-full p-2 text-center text-white bg-gray-400 rounded"
+              >
+                커뮤니티 만들기
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
