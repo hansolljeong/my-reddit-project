da2cc157247ac7246f2bd20c3861f79934fb5a1c
Hansol <losnah24@gmail.com>
2023-02-12 14:26:47 +0900

    이미지 라우팅, 업로드 기능 구현 중 에러

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Image from "next/image";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useAuthState } from "@/context/auth";
 
 const SubPage = () => {
   const fetcher = async (url: string) => {
@@ -13,14 +15,10 @@ const SubPage = () => {
     }
   };
 
-  // const fileInputRef = useRef<HTMLInputElement>(null);
-  // const openFileInput = (type: string) => {
-  //   const fileInput = fileInputRef.current;
-  //   if (fileInput) {
-  //     fileInput.name = type;
-  //     fileInput.click();
-  //   }
-  // };
+  const [ownSub, setOwnSub] = useState(false);
+  const { authenticated, user } = useAuthState();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const router = useRouter();
   const subName = router.query.sub;
@@ -31,57 +29,100 @@ const SubPage = () => {
   } = useSWR(subName ? `/subs/${subName}` : null, fetcher);
   console.log({ sub });
 
+  useEffect(() => {
+    if (!sub || !user) return;
+    setOwnSub(authenticated && user.username === sub.username);
+  }, [sub]);
+
+  const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files === null) return;
+
+    const file = event.target.files[0];
+    console.log("file", file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("type", fileInputRef.current!.name);
+
+    try {
+      await axios.post(`/subs/${sub.name}/upload`, formData, {
+        headers: { "Context-Type": "multipart/form-data" },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const openFileInput = (type: string) => {
+    const fileInput = fileInputRef.current;
+    if (fileInput) {
+      fileInput.name = type;
+      fileInput.click();
+    }
+  };
+
   return (
     <>
       {sub && (
         <>
-          {/* 배너 이미지 */}
-          <div className="bg-gray-400">
-            {sub.bannerUrl ? (
-              <div
-                className="h-56"
-                style={{
-                  backgroundImage: `url(${sub.bannerUrl})`,
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
-                // onClick={() => openFileInput("banner")}
-              ></div>
-            ) : (
-              <div
-                className="h-20 bg-gray-400"
-                // onClick={() => openFileInput("banner")}
-              ></div>
-            )}
-          </div>
-          {/* 커뮤니티 메타 데이터 */}
-          <div className="h-20 bg-white">
-            <div className="relative flex max-w-5xl px-5 mx-auto">
-              <div className="absolute" style={{ top: -15 }}>
-                {sub.imageUrl && (
-                  <Image
-                    src={sub.imageUrl}
-                    alt="커뮤니티 이미지"
-                    width={70}
-                    height={70}
-                    className="rounded-full"
-                    // onClick={() => openFileInput("image")}
-                  />
-                )}
-              </div>
-              <div className="pt-1 pl-24">
-                <div className="flex items-center">
-                  <h1 className="text-3xl font-bold ">{sub.title}</h1>
+          <div>
+            <input
+              type="file"
+              hidden={true}
+              ref={fileInputRef}
+              onChange={uploadImage}
+            />
+            {/* 배너 이미지 */}
+            <div className="bg-gray-400">
+              {sub.bannerUrl ? (
+                <div
+                  className="h-56"
+                  style={{
+                    backgroundImage: `url(${sub.bannerUrl})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                  onClick={() => openFileInput("banner")}
+                ></div>
+              ) : (
+                <div
+                  className="h-20 bg-gray-400"
+                  onClick={() => openFileInput("banner")}
+                ></div>
+              )}
+            </div>
+            {/* 커뮤니티 메타 데이터 */}
+            <div className="h-20 bg-white">
+              <div className="relative flex max-w-5xl px-5 mx-auto">
+                <div className="absolute" style={{ top: -15 }}>
+                  {sub.imageUrl && (
+                    <Image
+                      src={sub.imageUrl}
+                      alt="커뮤니티 이미지"
+                      width={70}
+                      height={70}
+                      className="rounded-full"
+                      onClick={() => openFileInput("image")}
+                    />
+                  )}
                 </div>
-                <p className="font-bold text-gray-400 text-small">
-                  /r/{sub.name}
-                </p>
+                <div className="pt-1 pl-24">
+                  <div className="flex items-center">
+                    <h1 className="text-3xl font-bold ">{sub.title}</h1>
+                  </div>
+                  <p className="font-bold text-gray-400 text-small">
+                    /r/{sub.name}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
           {/* 포스트와 사이드바 */}
-          <div className="flex max-w-5xl px-4 pt-5 mx-auto"></div>
+          <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+            {/* <div className="w-full md:mr-3 md:w-8/12">{renderPosts} </div>
+            <SideBar sub={sub} /> */}
+          </div>
         </>
       )}
     </>
