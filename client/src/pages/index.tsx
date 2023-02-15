7f09383130e00f0a84b489d66de0a191bcee60e6
Hansol <losnah24@gmail.com>
2023-02-15 20:42:22 +0900

    업무를 위한 카카오 싱크 테스트: 로그인 버튼 및 함수 생성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,9 +4,21 @@ import axios from "axios";
 import Link from "next/link";
 import useSWR from "swr";
 import Image from "next/image";
+import { FormEvent } from "react";
 
 export default function Home() {
   const { authenticated } = useAuthState();
+
+  // Kakao Login
+  const handleSubmitKakao = async (event: FormEvent) => {
+    try {
+      const res = await axios.get(
+        `/oauth/authorize?client_id=${process.env.REST_API_KEY}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code`
+      );
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
 
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
@@ -18,6 +30,12 @@ export default function Home() {
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       {/* 포스트 리스트 */}
       <div className="w-full md:mr-3 md:w-8/12"></div>
+      {/* 카카오 로그인 */}
+      <form onSubmit={handleSubmitKakao}>
+        <button className="w-full py-2 mb-1 text-xs font-bold text-gray uppercase bg-yellow-500 border border-gray-400 rounded">
+          카카오 로그인
+        </button>
+      </form>
       {/* 사이드바 */}
       <div className="hidden w-4/12 ml-3 md:block">
         <div className="bg-white border rounded">
